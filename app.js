a0c5ef6b0e54b38d2f681b9b822f5c09e3a7ef34
Ozan Aksoy <e233015@metu.edu.tr>
2022-06-18 02:47:45 +0300

    scrollreveal added

--- app.js
+++ app.js
@@ -1,11 +1,352 @@
-$('#carouselExampleCaptions').carousel({
-    interval: 0
-  })
+window.sr = new ScrollReveal();
 
-var offset = 80;
+sr.reveal('.ani1', {
+    delay: 200,
+    origin: "top",
+    distance: "200px",
+    interval: 100
+});
 
-  $('.navbar li a').click(function(event) {
-      event.preventDefault();
-      $($(this).attr('href'))[0].scrollIntoView();
-      scrollBy(0, 150);
-  });
+sr.reveal('.ani2', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri1', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri2', {
+  delay: 400,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri3', {
+  delay: 400,
+  origin: "left",
+  distance: "300px",
+  interval: 100
+});
+
+sr.reveal('.galeri4', {
+  delay: 400,
+  origin: "left",
+  distance: "300px",
+  interval: 100
+});
+
+sr.reveal('.galeri5', {
+  delay: 500,
+  origin: "left",
+  distance: "300px",
+  interval: 100
+});
+
+sr.reveal('.galeri6', {
+  delay: 500,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri7', {
+  delay: 500,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri8', {
+  delay: 500,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri9', {
+  delay: 500,
+  origin: "right",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri10', {
+  delay: 500,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri11', {
+  delay: 500,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri12', {
+  delay: 500,
+  origin: "right",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri13', {
+  delay: 500,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.galeri14', {
+  delay: 500,
+  origin: "top",
+  distance: "200px",
+  interval: 10
+});
+
+sr.reveal('.galeri15', {
+  delay: 500,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r1', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r2', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r3', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r4', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r5', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r6', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r7', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r8', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r9', {
+  delay: 300,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r10', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.r11', {
+  delay: 400,
+  origin: "right",
+  distance: "500px",
+  interval: 300
+});
+
+sr.reveal('.hak1', {
+  delay: 100,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.hak2', {
+  delay: 200,
+  origin: "top",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.hak3', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.hak4', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.hak5', {
+  delay: 300,
+  origin: "left",
+  distance: "200px",
+  interval: 100
+});
+
+sr.reveal('.hak6', {
+  delay: 300,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak7', {
+  delay: 300,
+  origin: "left",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak6', {
+  delay: 300,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak7', {
+  delay: 300,
+  origin: "left",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak8', {
+  delay: 300,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak9', {
+  delay: 300,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak10', {
+  delay: 300,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak11', {
+  delay: 300,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.hak12', {
+  delay: 300,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
+
+
+sr.reveal('.co1', {
+  delay: 200,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co2', {
+  delay: 300,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co3', {
+  delay: 300,
+  origin: "left",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co4', {
+  delay: 300,
+  origin: "left",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co5', {
+  delay: 600,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co6', {
+  delay: 600,
+  origin: "right",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co7', {
+  delay: 700,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
+
+sr.reveal('.co8', {
+  delay: 700,
+  origin: "top",
+  distance: "500px",
+  interval: 100
+});
